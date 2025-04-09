731dc7fbb1a22c690451a9b7570e417b7068b325
Diones Forteski <dionesforteski@gmail.com>
2025-04-09 14:42:44 -0300

    Ajustando Dockerfile para usar .NET 8 sem o <IsAspireSharedProject>true</IsAspireSharedProject>

--- OxfordOnline/obj/Debug/net8.0/OxfordOnline.AssemblyInfo.cs
+++ OxfordOnline/obj/Debug/net8.0/OxfordOnline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OxfordOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30a9b84d5985c0a6fe3ec53b35aeff3b60e5d7b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OxfordOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OxfordOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
