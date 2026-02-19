3266ae8b2c7e8fa11dd745f77d4d2d45135c7f74
Forteski-73 <dionesforteski@gmail.com>
2026-02-19 09:07:06 -0300

    endpoint for deleting images from the repository

--- OxfordOnline/obj/Debug/net8.0/OxfordOnline.AssemblyInfo.cs
+++ OxfordOnline/obj/Debug/net8.0/OxfordOnline.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OxfordOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25776e6b5cdca6faacee4419054b40339712abf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f0145d93bbfcb27182dab16883b2ab030bcf82")]
 [assembly: System.Reflection.AssemblyProductAttribute("OxfordOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OxfordOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
