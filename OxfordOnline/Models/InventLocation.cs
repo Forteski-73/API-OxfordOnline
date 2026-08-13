1ea27a8b5b6bfd4374d5a4e9dc671cfe9d361f96
Forteski-73 <dionesforteski@gmail.com>
2026-08-13 17:20:12 -0300

    New endpoint for Locations

--- OxfordOnline/Models/InventLocation.cs
+++ OxfordOnline/Models/InventLocation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace OxfordOnline.Models
 {
@@ -18,11 +19,12 @@ namespace OxfordOnline.Models
         public string InventLocationName { get; set; } = string.Empty;
 
         [Required]
+        [JsonIgnore]
         [Column("status")]
         public bool Status { get; set; } = true;
 
         #region Propriedades de Navegação
-
+        [JsonIgnore]
         public virtual ICollection<InventSum> InventSums { get; set; } = new List<InventSum>();
 
         #endregion
